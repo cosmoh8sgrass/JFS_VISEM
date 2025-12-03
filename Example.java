fb593243231245d45d4b9d353a786f7dde48f092
cosmoh8sgrass <kaushik.nadakuduti2k5@gmail.com>
2025-12-03 09:52:24 +0530

    Day 1

--- Example.java
+++ Example.java
@@ -1,5 +1,6 @@
 class Example {
 	public static void main (String [] args) {
-		System.out.println ("Welcome to SNB2"); 
+		System.out.println ("Welcome to SNB2");
+        System.out.println ("Day 1: GIT Overview"); 
 	}
 }
