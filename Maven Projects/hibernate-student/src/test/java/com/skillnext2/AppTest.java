8acad2b2814bd86da20e98830fd5ab53e2d7f50b
cosmoh8sgrass <kaushik.nadakuduti2k5@gmail.com>
2026-01-02 13:33:55 +0530

    Hibernate Student

---
+++ Maven Projects/hibernate-student/src/test/java/com/skillnext2/AppTest.java
@@ -0,0 +1,19 @@
+package com.skillnext2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
